418b6d0d7c9bbb2a8a6a9384fabea1c3da430360
unknown <lucashnevessousa@gmail.com>
2025-06-04 00:46:49 -0300

    Teste ja realizado. Git funcionando

--- Projeto_Oakley/resources/views/welcome.blade.php
+++ Projeto_Oakley/resources/views/welcome.blade.php
@@ -269,7 +269,7 @@
                 </div>
             </main>
         </div>
-    <h1>Testando</h1>
+        
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
